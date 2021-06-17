3261f7bc90df9615f5bc5a4fd826e56cfe549f3c
amir.in <amirsiddiqui.in@gmail.com>
2021-06-18 02:33:04 +0530

    style my list

--- src/components/UsersContainer.js
+++ src/components/UsersContainer.js
@@ -1,41 +1,66 @@
-import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
-import { fetchUsers } from '../redux'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../redux';
 
-function UsersContainer ({ userData, fetchUsers }) {
+function UsersContainer({ userData, fetchUsers }) {
+  //Calling UseEffect
   useEffect(() => {
-    fetchUsers()
-    console.log(userData)
-  }, [])
-  return userData.loading ? (
-    <h2>Loading</h2>
-  ) : userData.error ? (
-    <h2>{userData.error}</h2>
-  ) : (
+    fetchUsers();
+  }, []);
+
+  let loading = userData.loading;
+  let error = userData.error;
+  let users = userData.users;
+
+  return (
     <div>
-      <h2>API LIST</h2>
-      <div>
-        {userData &&
-          userData.users &&
-          userData.users.map(user => <p>Name : {user.name}<br/> Email : {user.email}<br/>User Name : {user.username}</p>)}
-      </div>
+      {loading && <div>Loader</div>}
+      {!loading && <div>{error && <div>Error hai bhai</div>}</div>}
+      {!loading && !error && (
+        <div>
+          <h2>API LIST</h2>
+          <div>
+            <div className="row">
+              {users.map((element, index) => {
+                return (
+                  <div className="col-md-3">
+                    <div
+                      style={{
+                        textAlign: 'left',
+                        border: '1px solid orange',
+                        margin: 5,
+                        borderRadius: 7,
+                        backgroundColor: '#bdbdbd',
+                      }}>
+                      <ul>
+                        <li style={{ paddingTop: 5, paddingBottom: 5, color: 'green', fontWeight: 700 }}>
+                          {element.name}
+                        </li>
+                        <li style={{ paddingTop: 5, paddingBottom: 5 }}>{element.email}</li>
+                        <li style={{ paddingTop: 5, paddingBottom: 5 }}>{element.phone}</li>
+                      </ul>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    userData: state.user
-  }
-}
+    userData: state.user,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
-    fetchUsers: () => dispatch(fetchUsers())
-  }
-}
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(UsersContainer)
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
